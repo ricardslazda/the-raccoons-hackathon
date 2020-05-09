0ae1b7b46807c5ba3cfeacbd3d2aa2e9c08cd263
unknown <ricardslazda28@gmail.com>
2020-05-09 12:37:27 +0300

    Created search bar

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -91,28 +91,10 @@
                 </div>
             </div>
             <div class="search">
-                <div class="search__hero">
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                    <div>
-                        <input required type="text" v-model="search" placeholder="Search asteroid ID"/>
-                        <button v-on:click="searchPlanet">Search</button>
+                    <div class="search-container">
+                        <input  v-model="search" id="searchBar" class="searchbar" type="text" placeholder="Search for Asteroid ID...">
+                        <a id="btnSearch" v-on:click="searchPlanet" class="btn-search"><i class="fa fa-search"></i></a>
                     </div>
-                </div>
             </div>
         </main>
     </div>
